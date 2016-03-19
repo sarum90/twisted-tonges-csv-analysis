1338f3dd4a8eff566d5070f47e38dfc34a651f66
Marcus Ewert <sarum90@gmail.com>
2016-03-19 09:57:53 -0700

    Fix the inttional error added to the tests.
    I intentionally added an error to the unit tests to verify that travis
    tests would fail and we would get a red badge on gitHub. I've confirmed
    that is all working correctly, so now I'll fix the test.

--- test.py
+++ test.py
@@ -5,9 +5,6 @@ class TestTravisSetUp(unittest.TestCase):
   def test_passes(self):
       self.assertEqual('foo', 'foo')
 
-  def test_fails(self):
-      self.assertEqual('foo', 'bar')
-
 if __name__ == '__main__':
     unittest.main()
 
